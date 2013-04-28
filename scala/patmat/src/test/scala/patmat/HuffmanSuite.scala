618bd6a7224f9bb6aa93376a4b763cb1ba11f1b1
dariusz.ostolski <dariusz.ostolski@gmail.com>
2013-04-28 21:19:18 +0200

    Added some implementation of Hufman code tree

--- scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -41,7 +41,8 @@ class HuffmanSuite extends FunSuite {
 
   test("decode and encode a very short text should be identity") {
     new TestTrees {
-      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+      val encoded = encode(t1)("ab".toList)
+      assert(decode(t1, encoded) === "ab".toList)
     }
   }
 }
